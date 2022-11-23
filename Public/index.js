6f97205ac32c2ecc83479d031d5c047e824589bc
Adrian Baltag <fbaltag83@gmail.com>
2022-11-23 10:34:35 -0500

    added animation for register btn form popup

--- Public/index.js
+++ Public/index.js
@@ -1,9 +1,21 @@
-// show form
+// show form login
 document.querySelector("#show-login").addEventListener("click", function(){
-    document.querySelector("form").classList.add("active");
+    document.querySelector("#form-login").classList.add("active");
 });
 
-// close form
-document.querySelector("form .close-btn").addEventListener("click", function(){
-    document.querySelector("form").classList.remove("active");
+// close form login
+document.querySelector("#form-login .close-btn").addEventListener("click", function(){
+    document.querySelector("#form-login").classList.remove("active");
+});
+
+
+// ========form register======
+// show form register
+document.querySelector("#show-register").addEventListener("click", function(){
+    document.querySelector("#form-register").classList.add("active1");
+});
+
+// close form register
+document.querySelector("#form-register .close-btn").addEventListener("click", function(){
+    document.querySelector("#form-register").classList.remove("active1");
 });

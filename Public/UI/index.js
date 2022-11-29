82a9dc14e96105b291790a219db693a7093371f4
Adrian Baltag <fbaltag83@gmail.com>
2022-11-29 13:32:49 -0500

    created form for user posts with animation

--- Public/UI/index.js
+++ Public/UI/index.js
@@ -1,5 +1,3 @@
-
-
 console.log("this js file is linked");
 // show form login
 document.querySelector("#show-login").addEventListener("click", function () {
@@ -27,6 +25,35 @@ document
     document.querySelector("#form-register").classList.remove("active1");
   });
 // ========end form-register================================================
+
+// ============form-create user post=======
+// ============form-create user post=======
+// show form login
+document.querySelector("#create_post").addEventListener("click", function () {
+  document.querySelector("#form-create").classList.add("active2");
+});
+
+// close form create user post
+document
+  .querySelector("#form-create .close-btn")
+  .addEventListener("click", function () {
+    document.querySelector("#form-create").classList.remove("active2");
+  });
+console.log("it's connected333");
+
+
+// btn dissapear on click
+        document.querySelector("#create_post").addEventListener("click", function() {
+            document.querySelector("#create_post").classList.add("dissapear")
+    })
+
+    // btn appear when form is closed
+
+    document
+    .querySelector("#form-create .close-btn")
+    .addEventListener("click", function () {
+      document.querySelector("#create_post").classList.remove("dissapear");
+    });
 
 // ======carousel================
 // const swiper = new Swiper(".mySwiper", {
---
+++ userPage.js
@@ -0,0 +1,80 @@
+
+// carousels
+let swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 20,
+    stretch: 0,
+    depth: 200,
+    modifier: 1,
+    slideShadows: true,
+    
+  },
+  loop: true,
+  autoplay: {
+    delay: 1500,
+    disableOnInteraction: false,
+    
+  },
+});
+
+let swiper1 = new Swiper(".mySwiper1", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  loop: true,
+  autoplay: {
+    delay: 500,
+    disableOnInteraction: false,
+  
+  },
+});
+
+
+
+
+
+
+
+
+
+
+
+// ============form-create user post=======
+// show form login
+document.querySelector("#create_post").addEventListener("click", function () {
+  document.querySelector("#form-create").classList.add("active2");
+});
+
+// close form create user post
+document
+  .querySelector("#form-create .close-btn")
+  .addEventListener("click", function () {
+    document.querySelector("#form-create").classList.remove("active2");
+  });
+console.log("it's connected333");
+
+
+// btn dissapear on click
+        document.querySelector("#create_post").addEventListener("click", function() {
+            document.querySelector("#create_post").classList.add("dissapear")
+    })
+
+    // btn appear when form is closed
+
+    document
+    .querySelector("#form-create .close-btn")
+    .addEventListener("click", function () {
+      document.querySelector("#create_post").classList.remove("dissapear");
+    });

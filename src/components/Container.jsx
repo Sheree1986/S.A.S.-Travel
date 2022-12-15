4f3e46ae0c5b9245395d158efc9b09f1b0801dec
Adrian Baltag <fbaltag83@gmail.com>
2022-12-14 21:01:30 -0500

    added btns for login/register pages with animations

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,29 +1,32 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import Cube from "./Cube";
+import CTA from "./CTA.jsx";
 
 const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#1a1a1a",
   ...theme.typography.body2,
   padding: theme.spacing(1),
-  textAlign: 'center',
+  textAlign: "center",
   color: theme.palette.text.secondary,
 }));
 
 export default function AutoGrid() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={3}>
-        <Grid item xs>
-          <Item>xs</Item>
+      <Grid container spacing={22}>
+        <Grid item xs={12}>
+          <Item>
+            <CTA />
+          </Item>
         </Grid>
-        <Grid item xs={6}>
-          <Item>xs=6</Item>
-        </Grid>
-        <Grid item xs>
-          <Item>xs</Item>
+        <Grid item xs={12}>
+          <Item>
+            <Cube />
+          </Item>
         </Grid>
       </Grid>
     </Box>

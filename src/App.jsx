47711e45aa8c600815d498b9dea4a959bc5c4a6f
Adrian Baltag <fbaltag83@gmail.com>
2022-12-14 21:11:13 -0500

    added register component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Container from "./components/Container";
-import Login from "./components/Login";
+// import Login from "./components/Login";
+// import Register from "./components/Register";
 
 function App() {
   return (
